06feba7a610f580d638105d3d13e4fe5df371e01
Jason Freeberg <jr.freeberg@comcast.net>
2017-02-28 21:03:07 -0800

    recursion working

--- wikiViz/wikiScrape.py
+++ wikiViz/wikiScrape.py
@@ -9,50 +9,96 @@
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from time import sleep
+import pandas as pd
 import re
 
 # Some constants
-baseURL = "https://en.wikipedia.org"
 URL = "https://en.wikipedia.org/wiki/Philosophy"
-wikiRe = re.compile(r"(?=(^/wiki))(?!.*(:))")
-
+baseURL = "https://en.wikipedia.org"
 # Grab main text and links
 html = urlopen(URL).read()
 soup = BeautifulSoup(html, "lxml")
-paragraphs = soup.findAll("p")
-links = soup.findAll("a")
-images = soup.findAll("img")
-sideTable = soup.find("table", attrs = {"class": "vertical-navbox"})
 
-#     Get info on paragraphs     #
-##################################
-citations = re.compile(r"\[\d+\]")
-for paragraph in paragraphs[:0]:
+# Article title
+def getTitle(soup):
+	titleJunk = re.compile(r" - Wikipedia")  # removes " - Wikipedia"
 
-	text = paragraph.get_text()
-	citations.sub(repl="", string=text)
+	title = soup.title.get_text()
+	title = titleJunk.sub(repl="", string=title)
+	return title
 
-###################################
+# Image for article
+def parseImages(soup):
+	parseImagesURL = re.complie(r"")
+	images = soup.findAll("img")
+	sideTable = soup.find("table", attrs = {"class": "vertical-navbox"})
+	tableImage = sideTable.find("img").get("src")
+	nImages = len(images)
 
-# Get links that match regex
-"""
-extensions = []
-for link in links:
-	href = str(link.get("href"))
-	title = str(link.get_text())
+	return tableImage, nImages
 
-	if wikiRe.match(href):
-		extensions.append(href)
+# Main body text
+def parseText(soup):
+	citations = re.compile(r"\[\d+\]")  # Removes citations
 
-extensions = list(set(extensions))
-extensions.sort()
+	paragraphs = soup.findAll("p")
+	corpus = ""
+	for paragraph in paragraphs:
+		text = str(paragraph.get_text())
+		noCites = citations.sub(repl="", string=text)
+		corpus += noCites
 
-for extension in extensions:
-	print(extension)
+	nWords = len(corpus.split())
+	nChar = len(corpus)
 
-# Get the image in sidebar
-tableImage = sideTable.find("img").get("src")
-print(sideTable.find("img"))
-print(tableImage)
-"""
+	return corpus, nWords, nChar
+
+
+def getLinksFromURL(aURL):
+	validLinks = re.compile(r"(?=(^/wiki))(?!.*(:))(?!.*(disambiguation))(?!.*(Main_Page))")
+
+	html = urlopen(aURL).read()
+	soup = BeautifulSoup(html, "lxml")
+	links = soup.findAll("a")
+	returnLinks = []  # list of extensions to return
+
+	for link in links:
+		href = str(link.get("href"))
+
+		if validLinks.match(href):
+			returnLinks.append(href)
+
+	return list(set(returnLinks))
+
+
+def getArticles(aURL, layer, extensionsSoFar):
+	baseURL = "https://en.wikipedia.org"
+
+	if layer <= 1 and len(extensionsSoFar) < 50000:
+		links = getLinksFromURL(aURL)
+		for link in links:
+			if link not in extensionsSoFar:
+				extensionsSoFar.append(link)
+				#print(len(extensionsSoFar), "extensions so far. Currently at layer", layer)
+				getArticles(baseURL + link, layer + 1, extensionsSoFar, layerCount)
+	else: # stop recursion
+		pass
+
+if __name__ == "__main__":
+	#idk = getLinksFromURL(baseURL + "/wiki/Philosophy")
+	lotsOfFuckingArticles = []
+	layerCount = []
+	getArticles(baseURL + "/wiki/Donald_Trump", 0, lotsOfFuckingArticles)
+
+	lotsOfFuckingArticles.sort()
+	print("-----------------------------------------------------", "\n \n \n \n\n\n")
+
+	print("Scraped", len(lotsOfFuckingArticles), "articles.")
+	print("Max layer =", max(layerCount))
+
+	for i in range(0, len(lotsOfFuckingArticles)):
+		print(lotsOfFuckingArticles[i], "\t\t\t\t\t\t", layerCount[i])
+
+
 
